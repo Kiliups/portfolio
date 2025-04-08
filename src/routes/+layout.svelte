65aa1487c9c27d7044c0fc1fdf075972a6bd91a8
Kilian Mayer <mayer-kilian@gmx.de>
2025-04-08 12:16:33 +0200

    init

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+	let { children } = $props();
+	import { ModeWatcher } from "mode-watcher";
+</script>
+
+<main class="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-16 lg:py-0">
+
+<ModeWatcher />
+{@render children?.()}
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import ModeSwitcher from "$lib/components/ui/ModeSwitcher.svelte";
+	import { m } from '$lib/paraglide/messages.js';
+    import { setLocale } from '$lib/paraglide/runtime';
+
+</script>
+    
+<h1>{m.hello_world({ name: 'SvelteKit User' })}</h1>
+<div>
+    <button onclick={() => setLocale('en')}>en</button>
+    <button onclick={() => setLocale('de')}>de</button>
+</div>
+<ModeSwitcher/>
+
+    
