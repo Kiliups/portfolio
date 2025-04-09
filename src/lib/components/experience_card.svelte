39c6f11dbadf805df8b13f5d163f995ade5bb8e7
Kilian Mayer <mayer-kilian@gmx.de>
2025-04-09 23:30:11 +0200

    added Badges to Experience Cards

--- src/lib/components/experience_card.svelte
+++ src/lib/components/experience_card.svelte
@@ -1,18 +1,27 @@
 <script lang="ts">
 	import { ChevronsUp } from '@lucide/svelte';
+	import { Badge } from './ui/badge';
 	import { Card } from './ui/card';
 
-	let { title, date, company, description = null, link = null } = $props();
+	let {
+		title,
+		date,
+		company,
+		description = null,
+		link = null,
+		badges = [],
+		className = null
+	} = $props();
 </script>
 
 <Card
-	class="group flex cursor-pointer flex-row items-baseline gap-8 border-none bg-transparent shadow-none lg:p-4 lg:hover:bg-slate-300/75 lg:hover:shadow-md lg:hover:dark:bg-slate-800/75"
+	class={`group flex ${link && 'cursor-pointer'} flex-row items-baseline gap-8 border-none bg-transparent shadow-none lg:p-4 lg:hover:bg-slate-300/75 lg:hover:shadow-md lg:hover:dark:bg-slate-800/75 ${className}`}
 	onclick={() => (link ? window.open(link, '_blank') : null)}
 >
 	<header>
 		<p class="whitespace-nowrap text-sm opacity-50">{date}</p>
 	</header>
-	<div class="text flex flex-col gap-1">
+	<div class="flex flex-col gap-1">
 		<div class="flex flex-row items-center gap-2 font-bold">
 			<h3>{title}</h3>
 			{#if link}
@@ -21,9 +30,18 @@
 				/>
 			{/if}
 		</div>
-
 		<p class="opacity-75">{company}</p>
 		<p class="text-sm opacity-75">{description}</p>
-		<div class="flex flex-row gap-2"></div>
+		{#if badges.length > 0}
+			<div class="flex flex-row flex-wrap gap-2">
+				{#each badges as badge}
+					<Badge
+						class="border-transparent bg-indigo-300/75 text-indigo-900/80 hover:bg-indigo-300/75"
+					>
+						<p>{badge}</p>
+					</Badge>
+				{/each}
+			</div>
+		{/if}
 	</div>
 </Card>
