b45a3b64c7b62ea6a4588106b67eea3f31b00a22
Kilian Mayer <mayer-kilian@gmx.de>
2026-02-15 14:31:25 +0100

    fix links

--- src/lib/components/experience_card.svelte
+++ src/lib/components/experience_card.svelte
@@ -10,20 +10,19 @@
 		badges = [],
 		className = null
 	} = $props();
+
+	let baseClass = $derived(
+		`group grid grid-cols-3 lg:hover:bg-base02/50 items-baseline gap-4 rounded border-none bg-transparent shadow-none lg:gap-8 lg:p-4 lg:hover:shadow-sm  ${className}`
+	);
 </script>
 
-<a
-	class={`group grid grid-cols-3 ${link && 'cursor-pointer'} lg:hover:bg-base02/50 items-baseline gap-4 rounded border-none bg-transparent shadow-none lg:gap-8 lg:p-4 lg:hover:shadow-sm  ${className}`}
-	href={link ? link : null}
-	target="_blank"
-	rel="noopener noreferrer"
->
+{#snippet cardContent()}
 	<header>
 		<p class="text-sm opacity-50">{date}</p>
 	</header>
 	<div class="col-span-2 flex flex-col gap-1">
 		<div class="flex flex-row items-center gap-2 font-bold">
-			<h3>{title}</h3>
+			<h3 class="w-full">{title}</h3>
 			{#if link}
 				<ChevronsUp
 					class="text-base07 rotate-45 opacity-0 transition-opacity group-hover:opacity-100"
@@ -44,4 +43,19 @@
 			</div>
 		{/if}
 	</div>
-</a>
+{/snippet}
+
+{#if link}
+	<a
+		class={`${baseClass} cursor-pointer`}
+		href={link ? link : null}
+		target="_blank"
+		rel="noopener noreferrer"
+	>
+		{@render cardContent()}
+	</a>
+{:else}
+	<div class={baseClass}>
+		{@render cardContent()}
+	</div>
+{/if}
